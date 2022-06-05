5e917fd4c52de97aebe1059ab87ab011751d4573
Developer <sharamradov@gmail.com>
2022-06-05 17:45:55 +0300

    end Auth Navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {Context} from "../index"
 import "../css/NavBar.scss"
 import "../css/main.scss"
-import { PICTURES_ROUTE, PAINTERS_ROUTE, SERVICES_ROUTE, ABOUTUS_ROUTE, CONTACTS_ROUTE, QUESTIONS_ROUTE } from '../utils/consts';
+import { PICTURES_ROUTE, PAINTERS_ROUTE, SERVICES_ROUTE, ABOUTUS_ROUTE, CONTACTS_ROUTE, QUESTIONS_ROUTE, LOGIN_ROUTE } from '../utils/consts';
 import {useNavigate} from "react-router-dom";
 import profilePic from "../images/profile-pic.jpg"
 import carPic from "../images/car-pic.jpg"
@@ -11,6 +11,13 @@ import {observer} from "mobx-react-lite";
 const NavBar = observer(() => {
     const navigate = useNavigate()
     const {user} = useContext(Context)
+
+    const logOut = () => {
+        user.setUser({})
+        user.setIsAuth(false)
+        localStorage.token = '';
+    }
+
     return (
         <div className="header">
             <div className="container-sm">
@@ -64,10 +71,25 @@ const NavBar = observer(() => {
                     <div className="header__user user">
                         {
                         user.isAuth ?
-                            <img alt="car" className="header__pic" src={carPic}/>
-                            
+                            <div className="user__status">
+                                <li className="header__item">
+                                    <span
+                                        onClick={()=>logOut()}
+                                        className={"header__link"}
+                                    >
+                                        Выйти
+                                    </span>
+                                </li>
+                                <div className="user__container-pic"><img alt="car" className="header__pic" src={carPic}/></div>
+                            </div>
                         :
-                            <img onClick={()=> user.setIsAuth(true)} alt="profile" className="header__pic" src={profilePic}/>
+                            <div className="user__container-pic">
+                                <img 
+                                    alt="profile" className="header__pic" 
+                                    src={profilePic}
+                                    onClick={() => navigate(LOGIN_ROUTE)}
+                                />
+                            </div>
                         }
                     </div>
                 </div>
