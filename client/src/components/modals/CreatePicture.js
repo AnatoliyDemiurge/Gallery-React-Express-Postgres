61ef44bd4cd9b64b5ec87c5aae054707a10e24c3
Developer <sharamradov@gmail.com>
2022-06-06 10:33:12 +0300

    add active inputs on CreatePicture AdminPanel

--- client/src/components/modals/CreatePicture.js
+++ client/src/components/modals/CreatePicture.js
@@ -1,8 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import {Modal, Button, Form, Dropdown} from "react-bootstrap"
 import {Context} from "../../index"
-const CreatePicture = ({show, onHide}) => {
+import { fetchGenres, fetchPainters} from '../../http/pictureAPI';
+import {observer} from "mobx-react-lite"
+const CreatePicture = observer(({show, onHide}) => {
     const {content} = useContext(Context)
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [description, setDescription] = useState('')
+    const [year, setYear] = useState(0)
+    const [size, setSize] = useState('')
+    const [file, setFile] = useState(null)
+
+    useEffect(()=>{
+        fetchGenres().then(data=> content.setGenres(data))
+        fetchPainters().then(data=> content.setPainters(data))
+    },[])
+
+    const selectFile = e => {
+        setFile(e.target.files[0])
+    }
     return (
         <Modal
             show={show}
@@ -16,11 +33,15 @@ const CreatePicture = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Form.Control
+                        value={name}
+                        onChange={e => setName(e.target.value)}
                         className="mt-3"
                         placeholder={"Введите название картины"}
                     >
                     </Form.Control>
                     <Form.Control
+                        value={price}
+                        onChange={e => setPrice(Number(e.target.value))}
                         className="mt-3"
                         placeholder={"Введите стоимость картины"}
                         type="number"
@@ -45,25 +66,36 @@ const CreatePicture = ({show, onHide}) => {
                         className="mt-3"
                         placeholder={"Загрузите фотографию картины"}
                         type="file"
+                        onChange={selectFile}
                     >
                     </Form.Control>
                     <Dropdown 
                         className="mt-3"
                     >
-                        <Dropdown.Toggle>Выберите Жанр</Dropdown.Toggle>
+                        <Dropdown.Toggle>{content.selectedGenre.name || "Выберите жанр"}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {content.genres.map(genre =>
-                                <Dropdown.Item key={genre.id}>{genre.name}</Dropdown.Item>
+                                <Dropdown.Item 
+                                    key={genre.id}
+                                    onClick={() => {content.setSelectedGenre(genre);console.log(content._selectedGenre)}}
+                                >
+                                    {genre.name}
+                                </Dropdown.Item>
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
                     <Dropdown 
                         className="mt-3"
                     >
-                        <Dropdown.Toggle>Выберите Художника</Dropdown.Toggle>
+                        <Dropdown.Toggle>{content.selectedPainter.name || "Выберите художника"}</Dropdown.Toggle>
                         <Dropdown.Menu>
-                            {content.painters.map(painter =>
-                                <Dropdown.Item key={painter.id}>{painter.name}</Dropdown.Item>
+                            {content.painters.map(painter =>    
+                                <Dropdown.Item
+                                    key={painter.id}
+                                    onClick={() => content.setSelectedPainter(painter)}
+                                >
+                                    {painter.name}
+                                </Dropdown.Item>
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
@@ -75,6 +107,6 @@ const CreatePicture = ({show, onHide}) => {
             </Modal.Footer>
         </Modal>
     )
-}
+})
 
 export default CreatePicture
